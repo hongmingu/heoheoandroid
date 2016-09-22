ffd5dd0622ff2252505a8336ed48546c378945df
Keepair-PC\Keepair <gmh397@nate.com>
2016-09-23 05:58:17 +0900

    기본예아전화가펑

--- app/src/main/java/com/example/keepair/myapplication/DetailActivity.java
+++ app/src/main/java/com/example/keepair/myapplication/DetailActivity.java
@@ -1,9 +1,42 @@
 package com.example.keepair.myapplication;
 
-/**
- * Created by Keepair on 2016-09-17.
- */
-public class DetailActivity {
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.keepair.myapplication.helper.Constants;
+import com.example.keepair.myapplication.model.Flower;
 
+public class DetailActivity extends AppCompatActivity {
+
+    private ImageView mImage;
+    private TextView mAuthor, mText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+
+        Flower flower = (Flower) intent.getSerializableExtra(Constants.REFERENCE.FLOWER);
+
+        configViews();
+
+        mAuthor.setText(flower.getAuthor());
+        mText.setText(flower.getText());
+
+        Glide.with(getApplicationContext()).load(flower.getImage()).into(mImage);
+
+    }
+
+    private void configViews() {
+        mImage = (ImageView) findViewById(R.id.iv_photo_detail);
+        mText = (TextView) findViewById(R.id.tv_text_detail);
+        mAuthor = (TextView) findViewById(R.id.tv_author_detail);
+    }
 }
